3d07d26f997885346a518cf0b1f55f8c02080143
JONAH-KIM-K <km33448379@gmail.com>
2026-06-17 14:09:40 +0900

    Add files via upload

---
+++ insunggumsa/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://insungtest-korea.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
